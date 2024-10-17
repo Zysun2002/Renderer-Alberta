3e38c8330cb34d8f62a3d920a670edb8d04dc6d7
Zysun(Sky) <145190598+Zysun2002@users.noreply.github.com>
2024-10-17 20:13:47 +0800

    Delete temp_tools directory

--- temp_tools/fix_obj.py
+++
@@ -1,8 +0,0 @@
-import trimesh
-import ipdb
-
-obj_path = "/root/autodl-tmp/tiny-renderer/assets/pumpkin/pumpkin_.obj"
-
-ipdb.set_trace()
-mesh = trimesh.load(obj_path, process=False)
-ipdb.set_trace()
